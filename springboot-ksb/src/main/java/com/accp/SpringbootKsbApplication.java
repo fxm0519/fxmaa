643fd9a33e4fc0537ebf632990ed238e4c21a60b
我爱你 <@DESKTOP-JHDV8UB>
2020-07-04 17:13:37 +0800

    思博第一次注释

--- springboot-ksb/src/main/java/com/accp/SpringbootKsbApplication.java
+++ springboot-ksb/src/main/java/com/accp/SpringbootKsbApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootKsbApplication {
-
+	/**
+	 * 
+	 * 我爱你中国
+	 * 
+	 * @param 思博
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootKsbApplication.class, args);
 	}
